48abb7454fff9b1366a51b4b5bf8a66e406bd78b
Taylor Handy <thandy1212@gmail.com>
2020-05-09 13:46:45 -0400

    Prepare workspace for node development with typescript

---
+++ src/util/secrets.ts
@@ -0,0 +1,11 @@
+import logger from './logger';
+import dotenv from 'dotenv';
+import fs from "fs";
+
+if (fs.existsSync(".env")) {
+    logger.debug("Using .env file to supply config enviornment variables");
+    dotenv.config({path: '.env'});
+}
+
+export const ENVIRONMENT = process.env.NODE_ENV;
+const prod = ENVIRONMENT === "production";
